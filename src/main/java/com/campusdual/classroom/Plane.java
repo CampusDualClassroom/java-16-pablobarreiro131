09f66595a6415c4527d903f6898731bb9956984c
Pablo <pablobarreiro131@gmail.com>
2025-05-23 09:03:07 +0200

    Ejercicio 16, completado y testeado.

--- src/main/java/com/campusdual/classroom/Plane.java
+++ src/main/java/com/campusdual/classroom/Plane.java
@@ -1,6 +1,6 @@
 package com.campusdual.classroom;
 
-public class Plane{
+public class Plane implements IMachine{
 
 	private final String name;
 
@@ -18,5 +18,21 @@ public class Plane{
 
 	public void fly() {
 		System.out.println("El avión está volando");
+	}
+
+
+	@Override
+	public void start() {
+		System.out.println("El avion esta encendido...");
+	}
+
+	@Override
+	public void stop() {
+		System.out.println("El avion esta apagado...");
+	}
+
+	@Override
+	public void maintenance() {
+		System.out.println("Se esta realizando el mantenimiento...");
 	}
 }
